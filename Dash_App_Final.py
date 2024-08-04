fde36e0d3b5d0784313eccc7403d313d5642b720
leko94 <ngcobo.nkululeko@yahoo.com>
2024-08-05 01:05:14 +0200

    Update Dash_App_Final.py

--- Dash_App_Final.py
+++ Dash_App_Final.py
@@ -659,4 +659,5 @@ def display_page(pathname):
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
